391e491049df342c71ba8a36b11610efb87b1b87
Eric Mendes Dantas <ericdantas0@gmail.com>
2017-09-12 15:49:55 -0300

    .

--- dist/alt-file-model.js
+++ dist/alt-file-model.js
@@ -11,7 +11,7 @@
           element.bind("change", function(changeEv) {
             var reader = new FileReader();
 
-            reader.onload = function(loadchangeEv) {
+            reader.onload = function(loadEv) {
               scope.$apply(function() {
                 scope.ngFileModel = {
                   lastModified: changeEv.target.files[0].lastModified,
@@ -23,12 +23,12 @@
                   files: changeEv.target.files,
 
                   // data === file in b64
-                  data: loadchangeEv.target.result
+                  data: loadEv.target.result
                 };
               });
             };
 
-            reader.readAsDataURL(changechangeEv.target.files[0]);
+            reader.readAsDataURL(changeEv.target.files[0]);
           });
         }
       };
